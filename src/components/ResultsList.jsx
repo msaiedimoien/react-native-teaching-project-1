fdeaa909c8e3c8015e6d03fae5fe13bd5e57c1a8
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-09-05 11:36:45 +0430

    Refactor project

--- src/components/ResultsList.jsx
+++ src/components/ResultsList.jsx
@@ -4,6 +4,9 @@ import {withNavigation} from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({title, results, navigation}) => {
+    if(!results.length)
+        return null;
+
     return (
         <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
--- src/screens/ResultsShowScreen.jsx
+++ src/screens/ResultsShowScreen.jsx
@@ -1,16 +1,55 @@
-import React from "react";
-import {View, Text, StyleSheet, Image} from "react-native";
+import React, {useEffect, useState} from "react";
+import {View, Text, StyleSheet, Image, FlatList, ScrollView} from "react-native";
+import Yelp from "../api/yelp";
 
 const ResultsShowScreen = ({navigation}) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
-    alert(id);
+
+    const getResult = async (id) => {
+        const response = await Yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+    if (!result) {
+        return null;
+    }
+
     return (
         <View>
-            <Text>Results Show Screen</Text>
+            <Text style={styles.title}>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({item}) => {
+                    return (
+                        <ScrollView>
+                            <Image style={styles.image} source={{uri: item}}/>
+                        </ScrollView>
+                    )
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 300,
+        height: 200,
+        marginLeft: 10,
+        marginBottom: 10,
+        borderRadius: 4,
+    },
+
+    title: {
+        fontWeight: "bold",
+        margin: 10,
+    },
+});
 
 export default ResultsShowScreen;
